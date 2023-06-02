3387e143f048d5525ff7d5f5f112b69dd7d2ea34
baranbicher <baranbicer0103@outlook.com>
2023-06-03 02:06:17 +0300

    Elemanların Kaç Tekrarı Var

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] list = {10, 20, 20, 10, 10, 20, 5, 20};
+        System.out.println("Dizi : " + Arrays.toString(list));
+        Arrays.sort(list);
+        System.out.println("Sıralı: " + Arrays.toString(list));
+        System.out.println("Tekrar Sayıları");
+        int counter = 1;
+        for (int i = 0; i < list.length; i++) {
+            if (i == 0) {
+                for (int j = 0; j < list.length; j++) {
+                    if ((i != j) && (list[i] == list[j])) {
+                        counter++;
+                    }
+                }
+                System.out.println(list[i] + " sayısı " + counter + " kere tekrar edildi.");
+            } else if (list[i] != list[i-1]) {
+                for (int j = 0; j < list.length; j++) {
+                    if ((i != j) && (list[i] == list[j])) {
+                        counter++;
+                    }
+                }
+                System.out.println(list[i] + " sayısı " + counter + " kere tekrar edildi.");
+            }
+            counter = 1;
+        }
+
+    }
+}
